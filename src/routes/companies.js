1a925f086fb2db5a7b613a30ace101cf50bbd82c
Alexei <bavelski@gmail.com>
2016-04-28 22:22:25 +0200

    periods

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -18,15 +18,42 @@ router.route('/api/companies/:symbol').get(function(req, res){
 });
 
 router.route('/api/history/:symbol').get(function(req, res) {
+	var period = req.query.period;
+	var symbol = req.params.symbol;
+	if (!period) {
+		period = '1y'
+	}
 	var startDate = new Date();
-	startDate.setFullYear(startDate.getFullYear()-1);
 
-	stockHistory.forSymbol(req.params.symbol)
-		.from(startDate).to(new Date()).getHistory(function(err, data){
+	var history = stockHistory.forSymbol(symbol);
+	if (period==='1y') {
+			startDate.setFullYear(startDate.getFullYear()-1);
+			history = history.from(startDate).to(new Date()).daily();
+	} else if (period==='5y') {
+			startDate.setFullYear(startDate.getFullYear()-5);
+			history = history.from(startDate).to(new Date()).weekly();
+	} else if (period==='all') {
+			startDate.setFullYear(startDate.getFullYear()-10);
+			history = history.from(startDate).to(new Date()).monthly();
+	} else if (period==='1m') {
+			startDate.setMonth(startDate.getMonth()-1);
+			history = history.from(startDate).to(new Date()).daily();
+	} else if (period==='3m') {
+			startDate.setMonth(startDate.getMonth()-3);
+			history = history.from(startDate).to(new Date()).daily();
+	} else if (period==='6m') {
+			startDate.setMonth(startDate.getMonth()-6);
+			history = history.from(startDate).to(new Date()).daily();
+	}
+
+	history.getHistory(function(err, data){
 			if (err) {
 				res.status(500).json({error: 'Internal error'}).end();
 			} else {
-				res.json(data).end();
+				res.json({
+					period: period,
+					points : data
+				}).end();
 			}
 
 	});
